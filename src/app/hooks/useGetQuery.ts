1b4085cbbce60b2ff0cbc95fd4bd0af4c1a6a198
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-11-07 10:03:25 -0400

    add try catch for error handling

--- src/app/hooks/useGetQuery.ts
+++ src/app/hooks/useGetQuery.ts
@@ -4,12 +4,20 @@ import { useEffect, useState } from "react";
 const useGetQuery = (search: string, category: string) => {
     const [products, setProducts] = useState<Product[]>([]);
     const fetchProducts = async () => {
-        const res = await fetch(
-            `https://api.escuelajs.co/api/v1/products/?title=${search}&categoryId=${category}`
-        );
-        const data = await res.json();
-        if (data) {
-            setProducts(data);
+        try {
+            const res = await fetch(
+                `https://api.escuelajs.co/api/v1/products/?title=${search}&categoryId=${category}`
+            );
+            const data = await res.json();
+            if (data) {
+                setProducts(data);
+            }
+            if (!data) {
+                console.log('error fetching data')
+            }
+        } catch (error) {
+            console.log(error)
+
         }
     };
 
